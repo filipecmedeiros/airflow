cbc8792fa51e4a7f4debabde2c6c70ca0561df58
Filipe Medeiros <filipecmedeiros88@gmail.com>
2022-09-26 20:47:49 -0300

    Implementing python operator

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,7 +1,29 @@
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+
 
 from datetime import datetime
+import json
+from pandas import json_normalize
+
+def _process_user(ti):
+    user = ti.xcom_pull(task_ids='extract_user')
+    user = user['results'][0]
+    processed_user = json_normalize(
+        {
+        'firstname': user['name']['first'],
+        'lastname': user['name']['last'],
+        'country': user['location']['country'],
+        'username': user['login']['username'],
+        'password': user['login']['password'],
+        'email': user['email']
+        }
+    )
+    processed_user.to_csv('tmp/processed_user.csv', index=None, header=False)
+
 
 with DAG('user_processing',
     start_date=datetime(2022, 9, 26),
@@ -21,6 +43,24 @@ with DAG('user_processing',
                 email TEXT NOT NULL);
         '''
     )
+
+    is_api_available = HttpSensor(
+        task_id='is_api_available',
+        http_conn_id = 'user_api',
+        endpoint = 'api/')
+
+    extract_user = SimpleHttpOperator(
+        task_id = 'extract_user',
+        http_conn_id = 'user_api',
+        endpoint = 'api/',
+        method = 'GET',
+        response_filter = lambda response: json.loads(response.text),
+        log_response = True)
+
+    process_user = PythonOperator(
+        task_id = 'process_user',
+        python_callable = _process_user)
+
 
 
 
