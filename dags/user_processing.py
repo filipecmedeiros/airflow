c4091887fb64ba89c93687bbb4fc415fff2b659c
Filipe Medeiros <filipecmedeiros88@gmail.com>
2022-09-26 21:23:50 -0300

    Fixing bug

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -14,17 +14,14 @@ from pandas import json_normalize
 def _process_user(ti):
     user = ti.xcom_pull(task_ids='extract_user')
     user = user['results'][0]
-    processed_user = json_normalize(
-        {
+    processed_user = json_normalize({
         'firstname': user['name']['first'],
         'lastname': user['name']['last'],
         'country': user['location']['country'],
         'username': user['login']['username'],
         'password': user['login']['password'],
-        'email': user['email']
-        }
-    )
-    processed_user.to_csv('tmp/processed_user.csv', index=None, header=False)
+        'email': user['email']})
+    processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
 
 
 def _store_user():
