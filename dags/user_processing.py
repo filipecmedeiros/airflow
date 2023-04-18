51cb7606212a0235b05d6e0f9af21ffb016c7940
Filipe Medeiros <filipecmedeiros88@gmail.com>
2023-04-18 16:02:58 -0300

    Adding DAG user_processing

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -34,7 +34,7 @@ def _store_user():
 
 
 with DAG('user_processing',
-    start_date=datetime(2022, 9, 26),
+    start_date=datetime(2023, 1, 1),
     schedule_interval='@daily',
     catchup=False) as dag:
     
